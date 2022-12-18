670aadade8a4ca69011b0ce04356fe409d26edb0
n-miyamoto <scotsdowneroad@gmail.com>
2022-12-18 23:03:36 +0900

    add ambient compatible endpoint.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,13 @@ pub mod schema;
 
 use crate::sensordata::NewSensorData;
 
+#[get("/")]
+fn hello_root() -> String {
+
+    println!("Hello, world! root");
+    "hello, world root!!".to_string()
+}
+
 #[get("/hello")]
 fn hello() -> String {
 
@@ -27,9 +34,12 @@ fn post_sensor_data(sensor_data: String) -> String {
     "OK".to_string()
 }
 
-#[post("/test_post", format = "json", data = "<test_post_data>")]
-fn test_post(test_post_data: String) -> String {
-    test_post_data
+//ambient compatible endpoint.
+#[post("/v2/channels/<channel>/data", format = "json", data = "<sensor_data>")]
+fn post_ambient_data(channel: u16, sensor_data: String) -> String {
+    println!("channel : {}", channel);
+    println!("{}", sensor_data);
+    "OK".to_string()
 }
 
 fn main() {
@@ -41,8 +51,9 @@ fn main() {
     //rocket = posts::mount(rocket);
     rocket.mount("/api",
     routes![
+        hello_root,
         hello,
         post_sensor_data,
-        test_post,
+        post_ambient_data,
     ]).launch();
 }
